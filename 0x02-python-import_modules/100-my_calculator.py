23bc631938124f139464d01ceb4170e54b60a6a4
Azbad <atbazeezd4917@gmail.com>
2024-01-16 09:16:55 -0800

    commit for 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     if operator == '+':
         print(f"{a} {operator} {b} = {add(a,b)}")
     elif operator == '-':
-        print("{} {) {} = {}".format(a, operator, b, sub(a,b)))
+        print(f"{a} {operator} {b} = {sub(a,b)}")
     elif operator == '*':
         print(f"{a} {operator} {b} = {mul(a,b)}")
     elif operator == 'div':
