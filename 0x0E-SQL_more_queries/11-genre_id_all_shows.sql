d1f2f92e37df4c20b217000bcfd3907fd43031d4
Azbad <atbazeezd4917@gmail.com>
2024-06-11 12:40:37 +0100

    commit for 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,9 +1,9 @@
---Lists all shows in the hbtn_0d_tvshows database with their genres
+ -- Lists all shows contained in the database hbtn_0d_tvshows
 -- Each record displays: tv_shows.title - tv_show_genres.genre_id
 -- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id
-  FROM tv_shows
-  LEFT JOIN tv_show_genres
-    ON tv_shows.id = tv_show_genres.show_id
- ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- If a show doesn’t have a genre, NULL is displayed
 
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
