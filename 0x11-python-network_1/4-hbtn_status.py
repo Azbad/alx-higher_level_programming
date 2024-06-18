01e5f04fb1afd1e234044d72763905b5a85e70c0
Azbad <atbazeezd4917@gmail.com>
2024-06-18 19:10:00 +0100

    commit for 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
+url = 'https://alx-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-        r = requests.get("https://alx-intranet.hbtn.io/status")
-            print("Body response:")
-                print("\t- type: {}".format(type(r.text)))
-                    print("\t- content: {}".format(r.text))
+response = requests.get(url)
+body = response.text
+
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
