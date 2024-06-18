21dd841702e626afa0f37746dc78c8b9f373d53f
Azbad <atbazeezd4917@gmail.com>
2024-06-18 21:51:00 +0100

    commit for 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,8 +8,8 @@ Usage: ./6-post_email.py <URL> <email>
 
 
   if __name__ == "__main__":
-          url = sys.argv[1]
-              value = {"email": sys.argv[2]}
+      url = sys.argv[1]
+      value = {"email": sys.argv[2]}
 
-                  r = requests.post(url, data=value)
-                      print(r.text)
+      r = requests.post(url, data=value)
+      print(r.text)
