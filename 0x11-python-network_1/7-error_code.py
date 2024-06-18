0d74dd24d98a93783cb49f9f336b12da7dc307d6
Azbad <atbazeezd4917@gmail.com>
2024-06-18 22:35:08 +0100

    commit for 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""
-Python script that takes in a URL, sends a request to the URL,
-and displays the body of the response. If the HTTP status code is greater
-than or equal to 400, it prints: Error code: followed by the value of
-the HTTP status code.
-"""
+"""Sends a request to a given URL and displays the response body.
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+  """
+  import sys
+  import requests
 
-import requests
-import sys
 
-if __name__ == "__main__":
-        url = sys.argv[1]
-            response = requests.get(url)
+  if __name__ == "__main__":
+      url = sys.argv[1]
 
-                if response.status_code >= 400:
-                            print("Error code:", response.status_code)
-                                else:
-                                            print(response.text)
+      r = requests.get(url)
+      if r.status_code >= 400:
+          print("Error code: {}".format(r.status_code))
+      else:
+          print(r.text)
