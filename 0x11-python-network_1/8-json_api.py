237656bdf57c22a53ec2bda0e648e7f24a2d43ce
Azbad <atbazeezd4917@gmail.com>
2024-06-19 04:49:30 -0700

    commit for 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as value of the variable `q`.
+  - If no letter is provided, send `q=""`.
 """
-Python script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter.
-"""
-
-import requests
 import sys
+import requests
+
 
 if __name__ == "__main__":
-        if len(sys.argv) == 1:
-                    q = ""
-                        else:
-                                    q = sys.argv[1]
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-                                        payload = {'q': q}
-                                            url = "http://0.0.0.0:5000/search_user"
-                                                response = requests.post(url, data=payload)
-
-                                                    try:
-                                                                data = response.json()
-                                                                        if data:
-                                                                                        print("[{}] {}".format(data.get('id'), data.get('name')))
-                                                                                                else:
-                                                                                                                print("No result")
-                                                                                                                    except ValueError:
-                                                                                                                                print("Not a valid JSON")
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
