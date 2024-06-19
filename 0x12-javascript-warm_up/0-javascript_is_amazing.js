f5914ea6b8371086392eec6ebf9f012c2a54b7a8
Azbad <atbazeezd4917@gmail.com>
2024-06-19 07:47:21 -0700

    commit for 0-javascript_is_amazing

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-
-// Creating a constant variable called myVar
-const myVar = "JavaScript is amazing";
-
-// Printing the value of myVar
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
