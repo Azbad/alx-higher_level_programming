de4f5e6e34657ac88ab4a319334c2a3ba708bac3
Azbad <atbazeezd4917@gmail.com>
2024-06-19 05:26:30 -0700

    commit for 100-githib_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that lists 10 commits (from the most recent to oldest)
+Python script which lists 10 commits (from the most recent to oldest)
 of a given repository by a given user using the GitHub API.
 """
 
@@ -8,15 +8,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
-        repository = sys.argv[1]
-            owner = sys.argv[2]
+    repository = sys.argv[1]
+    owner = sys.argv[2]
 
-                url = f"https://api.github.com/repos/{owner}/{repository}/commits"
-                    response = requests.get(url)
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
+    response = requests.get(url)
 
-                        commits = response.json()
+    commits = response.json()
 
-                            for commit in commits[:10]:
-                                        sha = commit.get('sha')
-                                                author_name = commit.get('commit').get('author').get('name')
-                                                        print(f"{sha}: {author_name}")
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        author_name = commit.get('commit').get('author').get('name')
+        print(f"{sha}: {author_name}")
